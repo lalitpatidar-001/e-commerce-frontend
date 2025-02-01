9f426b487bc7b8ae1633896a128c0e5f27745942
Lalit <lalitpatidar388@gmail.com>
2025-02-01 15:45:54 +0530

    Created slider

---
+++ src/app/shared/slider/slider.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.scss'
+})
+export class SliderComponent {
+  @Input() title!: string;
+}
